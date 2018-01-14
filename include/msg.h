5a716ed6932221b120ed2688767088635738380a
Ronan Boiteau <boiteauronan@gmail.com>
2018-01-14 03:07:20 +0100

    Improved error message

--- include/msg.h
+++ include/msg.h
@@ -5,7 +5,7 @@
 # define ERR_MALLOC		"Out of memory!\n"
 # define ERR_FORBID_CHAR	"Forbidden letter in dictionary: '*'\n"
 # define TOO_MANY_ARGS		"Too many arguments!\n"
-# define ERR_LETTER		"You have to input at least 1 letter!\n"
+# define ERR_LETTER		"Wrong input!\n"
 # define LETTER_NOT_FOUND       ": is not in the word.\n"
 # define NO_TRY_LEFT		"You loose...\n"
 # define MSG_WIN		"Congratulations!\n"
